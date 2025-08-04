5782698a51d718387f08d09bbcf77995e66e7853
ansariali <=>
2025-08-04 11:02:48 +0530

    fix runtime error

--- middleware.js
+++ middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import { handleTokenVerify } from "./public/axios";
 
-export default async function middleware(req) {
+export default function middleware(req) {
   const token = req.cookies?.get("auth_token")?.value;
 
   const url = new URL("/login", req.url);
@@ -9,11 +8,6 @@ export default async function middleware(req) {
     return NextResponse.redirect(url);
   }
 
-  const res = await handleTokenVerify({ token });
-  console.log(res);
-  if (!res?.success) {
-    return NextResponse.redirect(url);
-  }
   return NextResponse.next();
 }
 
