24839bfda6e3391735a124cd23c2e7f777ec791c
ansariali <=>
2025-08-04 11:54:34 +0530

    problem 2

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -39,7 +39,7 @@ export default function Login() {
 
     handleSuccessSwal(res?.message || "Login successfully");
     await handleAuth();
-    router.push("/dashboard");
+    router.push("/");
 
     setFormdata({ email: "", password: "" });
   };
