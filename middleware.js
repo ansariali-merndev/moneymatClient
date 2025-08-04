7faacfa3358e09d3fdb9c9b1898f630061e73c17
ansariali <=>
2025-08-04 12:06:06 +0530

    p5

--- middleware.js
+++ middleware.js
@@ -4,14 +4,14 @@ export default function middleware(req) {
   const token = req.cookies?.get("auth_token")?.value;
 
   console.log(token);
-  // const url = new URL("/login", req.url);
-  // if (!token) {
-  //   return NextResponse.redirect(url);
-  // }
+  const url = new URL("/login", req.url);
+  if (!token) {
+    return NextResponse.redirect(url);
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/"],
 };
