1479a04b30005b389e4560b17683caff9e1df0d1
ansariali <=>
2025-08-04 11:57:11 +0530

    p3

--- middleware.js
+++ middleware.js
@@ -3,10 +3,12 @@ import { NextResponse } from "next/server";
 export default function middleware(req) {
   const token = req.cookies?.get("auth_token")?.value;
 
-  const url = new URL("/login", req.url);
-  if (!token) {
-    return NextResponse.redirect(url);
-  }
+  // const url = new URL("/login", req.url);
+  // if (!token) {
+  //   return NextResponse.redirect(url);
+  // }
+
+  console.log(token);
 
   return NextResponse.next();
 }
