810cce5d6c61b6691ab46247288494770d211057
ansariali <=>
2025-08-04 11:24:53 +0530

    donewit --

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,11 +1,9 @@
-import { Authorized } from "@/components/Authorized";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 
 export default function RootLayout({ children }) {
   return (
     <>
-      <Authorized />
       <Header />
       {children}
       <Footer />
